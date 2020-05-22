e14ddb9f33219daa9de5d24d249050a7b9ea618c
khmirov <61903052+khmirov@users.noreply.github.com>
2020-05-22 14:25:49 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿/*Товар поступает на таможню.Ожидает проверки.
+Проходит проверку - покидает таможню. 
+Если не проходит проверку - помещается на специальных склад.
+Можно реализовать ещё и отдельно этот специальный склад.
+Подумайте, какие методы, классы, события можно реализовать.
+Нужно сделать описание классов в отдельный библиотеки классов и подключить её к тестовой программе.
+*/
+using System;
+using static System.Console;
+
+
+namespace TamozhnyaClassLib
+{
+    public class Check
+    {
+
+        public delegate void Show(string message);
+        public Show Message;
+        string product;
+       
+        public Check() 
+        { }
+
+        public Check (string product)
+        {
+            this.product = product;
+        }
+
+        public string InputProduct()
+        {
+            Console.Write("Input Your Product: ");
+            product = Console.ReadLine();
+            return product;
+        }
+
+        public bool CheckProduct ()
+        {
+                if (product == "drug" || product == "slave" || product == "gun" || product == "whore")
+                {
+                    Message?.Invoke($"Attention! Your product is {product}.");
+                    Message?.Invoke("It went to the storage area.");
+                    return false;
+                }
+                else
+                {
+                    Message?.Invoke($"OK. Next product, please.");
+                    return true;
+                }
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+/*Товар поступает на таможню.Ожидает проверки.
+Проходит проверку - покидает таможню. 
+Если не проходит проверку - помещается на специальных склад.
+Можно реализовать ещё и отдельно этот специальный склад.
+Подумайте, какие методы, классы, события можно реализовать.
+Нужно сделать описание классов в отдельный библиотеки классов и подключить её к тестовой программе.
+*/
+using System;
+using static System.Console;
+using TamozhnyaClassLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlTypes;
+
+namespace Tamozhnya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check nextProduct = new Check();
+            var storage = new List<string>();
+
+            nextProduct.Message = Show;
+            string input;
+            bool t;
+
+            do
+            {
+                input = nextProduct.InputProduct();
+                t = nextProduct.CheckProduct();
+                if (t == false)
+                    storage.Add(input);
+                WriteLine("\n");
+            } 
+            while (input != "end");
+
+
+            WriteLine("Storage area: ");
+            foreach (var item in storage)
+            {
+                WriteLine(item);
+            }
+
+           
+
+ 
+        }
+
+        public static void Show(string Message)
+        {
+            WriteLine(Message);
+        }
+    }
+}
